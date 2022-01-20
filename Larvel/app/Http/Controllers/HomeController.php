977dced3b360fdbd83592b018f4e6cb7669e088a
kristjanpaul <kristjan-paul@hanni.ee>
2022-01-20 19:57:23 +0200

    Initial commit

---
+++ Larvel/app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        return view('index');
+    }
+
+    public function articles(){
+        $articles = Article::latest()->paginate(16);
+
+        return view('articles', compact('articles'));
+    }
+
+    public function article(Article $article){
+
+        return view('article', compact('article'));
+    }
+    public function tag(Tag $tag){
+        $articles = $tag->articles()->latest()->paginate();
+        return view('articles', compact('articles'));
+    }
+
+    public function user(User $user){
+        $articles = $user->articles()->latest()->paginate();
+        return view('user', compact('user', 'articles'));
+    }
+}
+
+    
